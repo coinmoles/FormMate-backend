ca37dd803c3c3a3b9acee47c6a89f00968e3cc8c
coinmoles <andrewpark421@gmail.com>
2023-05-14 02:40:58 +0900

    Added getUser

--- src/routes/user/getUser.ts
+++ src/routes/user/getUser.ts
@@ -1,28 +1,34 @@
 import { Next } from "koa"
-import Ajv, { JSONSchemaType } from "ajv"
-import { CustomContext } from "src/util/interface/KoaRelated"
-
-const ajv = new Ajv()
-
-interface Ctx {
-
-}
-
-const schema: JSONSchemaType<Ctx> = {
-    type: "object",
-    properties: {
-
-    },
-    required: [],
-    additionalProperties: true
-}
-
-const validateBody = ajv.compile(schema)
+import { client } from "../../db/client"
+import { CustomContext } from "../../util/interface/KoaRelated"
+import { GetItemCommand, ResourceNotFoundException } from "@aws-sdk/client-dynamodb"
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
 
 export const getUser = async (ctx: CustomContext, next: Next): Promise<void> => {
-    if (!validateBody(ctx.request.body)) {
-        ctx.response.status = 400
+    const user = ctx.request.user
+    if ("errorType" in user) {
+        const errorType = user.errorType
+        if (errorType === "Token Expired") {
+            ctx.response.status = 401
+            ctx.response.message = "Login Expired"
+            return next()
+        }
+        else if (errorType === "User Not Found" || errorType === "Wrong Token") {
+            ctx.response.status = 401
+            ctx.response.message = "No login data"
+            return next()
+        }
+        else {
+            ctx.response.status = 500
+            ctx.response.message = "Unknown Error"
+            return next()
+        }
     }
-    
+
+    const userAny: any = user
+    delete userAny.password
+    ctx.response.status = 200
+    ctx.response.message = "Success"
+    ctx.response.body = userAny
     return next()
 }
--- src/routes/user/getUserById.ts
+++ src/routes/user/getUserById.ts
@@ -1,28 +1,38 @@
 import { Next } from "koa"
-import Ajv, { JSONSchemaType } from "ajv"
-import { CustomContext } from "src/util/interface/KoaRelated"
-
-const ajv = new Ajv()
-
-interface Ctx {
-
-}
-
-const schema: JSONSchemaType<Ctx> = {
-    type: "object",
-    properties: {
-
-    },
-    required: [],
-    additionalProperties: true
-}
-
-const validateBody = ajv.compile(schema)
+import { client } from "../../db/client"
+import { CustomContext } from "../../util/interface/KoaRelated"
+import { GetItemCommand, ResourceNotFoundException } from "@aws-sdk/client-dynamodb"
+import { marshall, unmarshall } from "@aws-sdk/util-dynamodb"
 
 export const getUserById = async (ctx: CustomContext, next: Next): Promise<void> => {
-    if (!validateBody(ctx.request.body)) {
-        ctx.response.status = 400
+    const { userid }: { userid: string } = ctx.params
+    
+    let result
+    try {
+        result = await client.send(new GetItemCommand({
+            TableName: "User",
+            Key: marshall({
+                userId: userid
+            })
+        }))
+    } catch(err) {
+        if (!(err instanceof ResourceNotFoundException)) {
+            console.log(err)
+            ctx.response.status = 500
+            ctx.response.message = "Unknown Error"
+            return next()
+        }
+    }
+    if (!result || !result.Item) {
+        ctx.response.status = 404
+        ctx.response.message = "User not found"
+        return next()
     }
     
+    const user = unmarshall(result.Item)
+    delete user.password
+    ctx.response.status = 200
+    ctx.response.message = "Success"
+    ctx.response.body = user
     return next()
 }
